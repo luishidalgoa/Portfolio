a0b52e9d1e0a67a9554edde90d238caf39611783
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2025-04-08 21:55:26 +0200

    Update experiencia.component.ts

--- src/app/Home/experiencia/experiencia.component.ts
+++ src/app/Home/experiencia/experiencia.component.ts
@@ -12,7 +12,7 @@ export class ExperienciaComponent {
       puesto: 'Intern Junior Software Developer',
       company: 'DeUser',
       description: 'Durante el periodo de practicas he desarrollado un rol de automatización de procesos internos en la empresa, usando herramientas de la switft de Microsoft 365 como Power Automate, Power Apps y Share Point. He desarrollado una aplicación para crear etiquetas de productos para posteriormente imprimirlas. De esta manera los encargados del almacen podran identificar los productos de manera mas rapida y eficiente.',
-      companyLogo: 'http://minio.luishidalgoa.ddns-ip.net:9001/api/v1/buckets/portfolio/objects/download?preview=true&prefix=shared%2Fdeuser_logo.jpg&version_id=null'
+      companyLogo: 'https://minio.luishidalgoa.ddns-ip.net/api/v1/buckets/portfolio/objects/download?preview=true&prefix=shared%2Fdeuser_logo.jpg&version_id=null'
     },
     {
       startDate: new Date(2024, 3),
